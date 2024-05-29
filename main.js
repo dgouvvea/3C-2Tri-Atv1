192419ea1e95801a2513376a95b728dd0e750707
dgouvvea <81660341+dgouvvea@users.noreply.github.com>
2024-05-29 07:57:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const cxPrincipal = document.querySelector(".cx-principal");
+const cxPerguntas = document.querySelector(".cx-perguntas");
+const cxAlternativas = document.querySelector(".cx-alternativas");
+const cxResultado = document.querySelector(".cx-resultado");
+const txResultado = document.querySelector(".tx-resultado");
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]; 
+

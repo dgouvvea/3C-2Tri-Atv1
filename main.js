48d8bdc3f2b33a81af05a7e40d4e96ed5ab6a273
dgouvvea <81660341+dgouvvea@users.noreply.github.com>
2024-07-31 08:21:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,62 @@
-const cxPrincipal = document.querySelector(".cx-principal");
-const cxPerguntas = document.querySelector(".cx-perguntas");
-const cxAlternativas = document.querySelector(".cx-alternativas");
-const cxResultado = document.querySelector(".cx-resultado");
-const txResultado = document.querySelector(".tx-resultado");
-const perguntas = [
+var cxPrincipal = document.querySelector(".cx-principal");
+var cxPerguntas = document.querySelector(".cx-perguntas");
+var cxAlternativas = document.querySelector(".cx-alternativas");
+var cxResultado = document.querySelector(".cx-resultado");
+var txResultado = document.querySelector(".tx-resultado");
+var perguntas = [
     {
-        enunciado: "Pergunta 1",
-        alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+        enunciado: "A IA pode automatizar tarefas repetitivas.",
+        alternativas: [    
+            "Vai eliminar tarefas monotonas",
+            "Vai tirar muitos empregos"
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "A IA pode tomar decisões baseadas em dados.",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Maravilhoso",
+            "Assustador"
+        ]
+    },
+    {
+        enunciado: "A IA pode substituir certos empregos.",
+        alternativas: [
+            "Não vejo problemas",
+            "É o fim dos empregos como conhecemos hoje"
+        ]
+    },
+    {
+        enunciado: "A IA pode ajudar a prever tendências de mercado.",
+        alternativas: [
+           
+            "Incrível",
+            "Sinistro"
+        ]
+    },
+    {
+        enunciado: "A IA pode trabalhar 24/7 sem pausas.",
+        alternativas: [
+            "Surpreendente",
+            "Mão de obra barata"
         ]
     }
-]; 
+];
 
+var posiçãoAtual = 0;
+var perguntaAtual;
+
+mostraPergunta();
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[posiçãoAtual];
+    cxPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (var alternativa of perguntaAtual.alternativas) {
+        var botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        cxAlternativas.appendChild(botaoAlternativas);
+    }
+}
